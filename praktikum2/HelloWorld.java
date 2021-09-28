72ea5f454c8b08f505e2359c817c3ccf784b8201
ariamuhammad77@gmail.com <ariamuhammad77@gmail.com>
2021-09-28 22:33:13 +0700

    HelloWorld.java

---
+++ praktikum2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	
+    public static void main(String[] args) {
+	    System.out.println("Nama : Aria Muhammad Luthfi!");
+		System.out.println("Nim  : D3RPLA6706213152");
+		System.out.println("Kelas: 45-01");
+	}
+}
+	
